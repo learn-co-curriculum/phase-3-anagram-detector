7c97cddc9dfdc0ef74a4a379d295b3dc72ac3b68
Alex Guanipatin <sebas5950@gmail.com>
2022-06-29 13:17:54 -0400

    commit tree 757e1e8da9cf3454d1cb03f8bb3d7aeea58f94a7

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,17 @@
-# Your code goes here!
+require 'pry'
+
+class Anagram
+
+    attr_accessor :match
+
+    def initialize(match)
+        @match = match
+    end
+
+    def print
+        puts "match: \n#{@match}"
+    end
+end
+
+word = Anagram.new("whats up")
+binding.pry
